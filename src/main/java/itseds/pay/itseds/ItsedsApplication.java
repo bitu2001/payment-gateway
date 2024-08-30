bdf34f5cddcc87cd8a59ad9831725298ded0e091
Bitu <tripathysuvendu5@gmail.com>
2024-08-30 20:02:38 +0530

    first commit

---
+++ src/main/java/itseds/pay/itseds/ItsedsApplication.java
@@ -0,0 +1,13 @@
+package itseds.pay.itseds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsedsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itseds/pay/itseds/ItsedsApplicationTests.java
@@ -0,0 +1,13 @@
+package itseds.pay.itseds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
